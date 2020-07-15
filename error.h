f6b00fc455b5925af084eb775f3eabdfaf623f23
thehiddenmonkey <gian.s.brar@gmail.com>
2020-07-14 22:27:16 -0400

    License and error

--- error.h
+++ error.h
@@ -3,7 +3,7 @@
 
 enum errorTypes {Generic, Interpreter, Warning};
 
-void err(short int cWarn, char *message) {
+void err(short int cWarn, const char *message) {
 	if (!cWarn) printf("\033[1;31mERROR: \033[0m");
 	else if (cWarn == 1) printf("\033[0;31mINTERPRETER ERROR: \033[0m");
 	else printf("[01;33mWARNING: \033[0m");
--- interpreter.cpp
+++ interpreter.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 #include <string>
-#include "stringops.h"
+#include <fstream>
+#include "error.h" // err() and errorTypes
 
 void interpreter() {
 	std::string input;
-	std::cout << ">> Scatterbrain CNS: Central Nervous System interpreter V0.0.1 ALPHA\nCopyrighted under the M.I.T. license (\"copyright\" for more info)." << std::endl;
+	std::cout << ">> Scatterbrain CNS: Central Nervous System interpreter V0.0.1 ALPHA\nCopyrighted under the M.I.T. license (\"copyright\" for more info).\n";
 	for (;;) {
-		std::cout << "";
+		std::cout << ">> ";
 		getline(std::cin, input);
+		//for (; *p; ++p) *p = tolower(*p);
 		// Following are all interpreter interface-specific routines, ending with the call to the main processing function
+		if (input == "copyright") { // TODO: Make strip function
+			std::ifstream license("LICENSE");
+			std::string line;
+			if (license.is_open()) while (getline(license, line)) std::cout << line << std::endl;
+			else err(Interpreter, "Could not open \"LICENSE\" ...where did our license go?");
+		}
 	}
 	return;
+}
+
+int main() {
+	interpreter();
+	return 0;
 }
