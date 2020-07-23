f3d74e885e3853f6ba5b90d4f1d5ec5d0685908f
thehiddenmonkey <gian.s.brar@gmail.com>
2020-07-23 10:53:55 -0400

    Formatting and included 'cstring'

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1,6 +1,7 @@
 #include <iostream> // std::cout, std::cin, and std::endl
 #include <string> // std::string and getline()
 #include <algorithm> // std::transform()
+#include <cstring> // std::strncmp()
 #include <fstream> // std::ifstream and is_open()
 #include "C_Headers/error.h" // err() and errorTypes
 #include "C_Headers/commands.h" // help()
@@ -17,14 +18,14 @@ void interpreter() {
 		getline(std::cin, initInput);
 		char* input = strip(lower(initInput.c_str())); // input is lowercase version
 		// Following are all interpreter interface-specific routines, ending with the call to the main processing function
-		if (std::strncmp(input, "copyright", 9) == 0) {
+		if (!std::strncmp(input, "copyright", 9)) {
 			std::ifstream license("../LICENSE");
 			std::string line;
 			if (license.is_open()) while (getline(license, line)) std::cout << line << std::endl;
 			else err(Interpreter, "Could not open \"LICENSE\" ...where did our license go?");
 			continue;
 		}
-		if (std::strncmp(input, "help", 4) == 0) help(1);
+		if (!std::strncmp(input, "help", 4)) help(1);
 		readLn(initInput.c_str());
 	}
 	return;
